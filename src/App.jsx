de55252c4e5d17dbc9ac26f4be99260ca3a5e45c
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-09-23 09:41:00 -0700

    Restore Yourkly-native project routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ import BrandWordmark from './components/BrandWordmark'
 // Auth pages (no shell)
 import AuthCallback from './pages/AuthCallback'
 import Welcome      from './pages/Welcome'
+import NativeJoin from './pages/NativeJoin'
+import NativeNewProject from './pages/NativeNewProject'
+import NativeProjects from './pages/NativeProjects'
+import NativeProjectHome from './pages/NativeProjectHome'
 
 // Global pages
 import Home        from './pages/Home'
